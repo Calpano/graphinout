8b9d6382e547f4ac03f49878d32dcf00fc47d99b
Dr. Max Völkel <mv@maxvoelkel.de>
2023-01-27 10:59:28 +0100

    add mock inputsource/outputsink

--- base/src/main/java/com/calpano/graphinout/base/input/InputSource.java
+++ base/src/main/java/com/calpano/graphinout/base/input/InputSource.java
@@ -1,11 +1,41 @@
 package com.calpano.graphinout.base.input;
 
+import com.calpano.graphinout.base.util.GIOUtil;
+
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringReader;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 
 public interface InputSource {
+
+    static InputSource of(String name, String content) {
+        return new InputSource() {
+            @Override
+            public Optional<Charset> encoding() {
+                return Optional.of(StandardCharsets.UTF_8);
+            }
+
+            @Override
+            public Optional<String> inputFormat() {
+                return Optional.empty();
+            }
+
+            @Override
+            public InputStream inputStream() throws IOException {
+                return
+                        new ByteArrayInputStream(content.getBytes(StandardCharsets.UTF_8));
+            }
+
+            @Override
+            public String name() {
+                return name;
+            }
+        };
+    }
 
     /** can be called repeatedly, but users need to close {@link InputStream after each usage */
     Optional<Charset> encoding();
--- base/src/main/java/com/calpano/graphinout/base/output/OutputSink.java
+++ base/src/main/java/com/calpano/graphinout/base/output/OutputSink.java
@@ -7,6 +7,25 @@ import java.util.Map;
 
 public interface OutputSink {
 
+    static OutputSink createMock() {
+        return new OutputSink() {
+            @Override
+            public OutputStream outputStream() throws IOException {
+                return System.out;
+            }
+
+            @Override
+            public List<String> readAllData() throws IOException {
+                return null;
+            }
+
+            @Override
+            public Map<String, Object> outputInfo() {
+                return null;
+            }
+        };
+    }
+
     /** can be called once. Users need to close {@link OutputStream after usage */
     OutputStream outputStream() throws IOException;
 
