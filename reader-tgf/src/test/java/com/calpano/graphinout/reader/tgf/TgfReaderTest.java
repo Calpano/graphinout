9d5f5e97ad048b5119b2e0212013cfa7f8d86f5b
Dr. Max Völkel <mv@maxvoelkel.de>
2023-01-27 11:00:47 +0100

    refactor TGF reader to new architecture

---
+++ reader-tgf/src/test/java/com/calpano/graphinout/reader/tgf/TgfReaderTest.java
@@ -0,0 +1,29 @@
+package com.calpano.graphinout.reader.tgf;
+
+import com.calpano.graphinout.base.gio.GioWriter;
+import com.calpano.graphinout.base.gio.GioWriterImpl;
+import com.calpano.graphinout.base.graphml.GraphmlWriterImpl;
+import com.calpano.graphinout.base.input.InputSource;
+import com.calpano.graphinout.base.output.OutputSink;
+import com.calpano.graphinout.base.output.xml.file.XMLFileWriter;
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+class TgfReaderTest {
+
+    @Test
+    void test() throws IOException {
+        String resourceName = "/example.tgf";
+        String content = IOUtils.resourceToString(resourceName, StandardCharsets.UTF_8);
+        InputSource inputSource = InputSource.of(resourceName,content);
+        OutputSink outputSink = OutputSink.createMock();
+
+        TgfReader tgfReader = new TgfReader();
+        GioWriter gioWriter = new GioWriterImpl(new GraphmlWriterImpl(new XMLFileWriter(outputSink)));
+        tgfReader.read(inputSource, gioWriter);
+    }
+
+}
