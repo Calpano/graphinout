c2eb520e22201763df68066fee6342d433b94882
lilladiamantisz <ldiamantisz@le-soft.io>
2023-01-30 17:06:12 +0100

    add actual parsing to tgf reader class

--- reader-tgf/src/main/java/com/calpano/graphinout/reader/tgf/TgfReader.java
+++ reader-tgf/src/main/java/com/calpano/graphinout/reader/tgf/TgfReader.java
@@ -1,14 +1,15 @@
 package com.calpano.graphinout.reader.tgf;
 
-import com.calpano.graphinout.base.gio.GioWriter;
+import com.calpano.graphinout.base.gio.*;
 import com.calpano.graphinout.base.input.InputSource;
-import com.calpano.graphinout.base.output.GraphMlWriter;
 import com.calpano.graphinout.base.reader.GioFileFormat;
 import com.calpano.graphinout.base.reader.GioReader;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.Scanner;
 import java.util.function.Consumer;
@@ -17,8 +18,13 @@ import static org.slf4j.LoggerFactory.getLogger;
 
 public class TgfReader implements GioReader {
     private static final Logger log = getLogger(TgfReader.class);
-    /** @Nullable TODO maybe make a true Optional */
-    private  Consumer<ContentError> errorConsumer;
+    public static final String DELIMITER = " ";
+    public static final String HASH = "#";
+    public static final String LABEL = "label";
+    /**
+     * @Nullable TODO maybe make a true Optional
+     */
+    private Consumer<ContentError> errorConsumer;
 
     @Override
     public GioFileFormat fileFormat() {
@@ -26,37 +32,66 @@ public class TgfReader implements GioReader {
     }
 
     public void errorHandler(Consumer<ContentError> errorConsumer) {
-        this.errorConsumer=errorConsumer;
+        this.errorConsumer = errorConsumer;
     }
-
 
     @Override
     public void read(InputSource inputSource, GioWriter writer) throws IOException {
-        // FIXME ...do actual parsing
-
         String content = IOUtils.toString(inputSource.inputStream());
 
         boolean isValid = false;
         Scanner scanner = new Scanner(content);
         boolean foundHash = false;
         int lineCount = 0;
+        boolean nodes = true;
+
+        writer.startDocument(GioDocument.builder().build());
+        writer.startGraph(GioGraph.builder().build());
 
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
             lineCount++;
-            if (line.contains("#")) {
+
+            if (nodes) {
+                String[] nodeParts = line.split(DELIMITER);
+
+                List<GioData> gioDataList = new ArrayList<>();
+                GioData gioData = GioData.builder().key(LABEL).value(nodeParts[1]).build();
+                gioDataList.add(gioData);
+
+                writer.startNode(GioNode.builder()
+                        .id(nodeParts[0])
+                        .dataList(gioDataList)
+                        .build());
+
+            }
+            if (line.contains(HASH)) {
                 foundHash = true;
+                nodes = false;
+            } else {
+                String[] edgeParts = line.split(DELIMITER);
+                GioEndpoint sourceEndpoint = GioEndpoint.builder().id(edgeParts[0]).build();
+                GioEndpoint targetEndpoint = GioEndpoint.builder().id(edgeParts[1]).build();
+
+                List<GioEndpoint> endpointList = new ArrayList<>();
+                endpointList.add(sourceEndpoint);
+                endpointList.add(targetEndpoint);
+                if (edgeParts.length == 3) {
+                    writer.startEdge(GioEdge.builder().endpoints(endpointList)
+                            .desc(edgeParts[2]).build());
+                }
+                writer.startEdge(GioEdge.builder().endpoints(endpointList).build());
             }
         }
+        writer.endGraph();
+        writer.endDocument();
         if (lineCount < 3 || !foundHash) {
             isValid = true;
         }
         scanner.close();
 
-        if(!foundHash && errorConsumer!=null) {
-            errorConsumer.accept(new ContentError(ContentError.ErrorLevel.Error,"Found no hash sign", Optional.empty()));
+        if (!isValid && errorConsumer != null) {
+            errorConsumer.accept(new ContentError(ContentError.ErrorLevel.Error, "TGF file is not valid.", Optional.empty()));
         }
-
-
     }
 }
