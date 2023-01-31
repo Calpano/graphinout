3c0736208f9883c887b48e0c513e63ccf4bdfc55
Rasoul Babaei <rbabaei@le-soft.io>
2023-01-31 09:29:25 +0100

    GraphmlWriter Interface implementation along with testing

--- base/src/main/java/com/calpano/graphinout/base/output/xml/GioGraphInOutXMLConstants.java
+++ base/src/main/java/com/calpano/graphinout/base/output/xml/GioGraphInOutXMLConstants.java
@@ -1,9 +1,13 @@
 package com.calpano.graphinout.base.output.xml;
 
+import com.calpano.graphinout.base.output.GraphMlWriter;
+import com.calpano.graphinout.base.output.OutputSink;
+import com.calpano.graphinout.base.output.xml.file.XMLFileWriter;
+
 public class GioGraphInOutXMLConstants {
+
     public static final String START_LOG = "#####--------start-------";
     public static final String END_LOG = "--------End-------#####";
-
     public static final String EDGE_ELEMENT_NAME = "edge";
     public static final String ENDPOINT_ELEMENT_NAME = "endpoint";
     public static final String EDGE_DATA_ELEMENT_NAME = "data";
@@ -15,4 +19,17 @@ public class GioGraphInOutXMLConstants {
     public static final String NODE_DATA_ELEMENT_NAME = "data";
     public static final String PORT_ELEMENT_NAME = "port";
     public static final String DESC_ELEMENT_NAME = "desc";
+    public static final String DEFAULT_ELEMENT_NAME = "Default";
+
+    public static GraphMlWriter ofXML(XmlWriter xmlWriter) {
+        return new GraphMlXmlWriter(xmlWriter);
+    }
+
+    public static XmlWriter ofFile(OutputSink outputSink) {
+        return new XMLFileWriter(outputSink);
+    }
+
+    public static GraphMlWriter ofXML(OutputSink outputSink) {
+        return ofXML(ofFile(outputSink));
+    }
 }
