8fc410b97748bc95e181dfdbb86515ff1476f8fe
Dr. Max Völkel <mv@maxvoelkel.de>
2023-02-03 11:24:13 +0100

    use slf4j Logger

--- reader-dot/src/main/java/com/calpano/graphinout/reader/dot/text/DotTextReader.java
+++ reader-dot/src/main/java/com/calpano/graphinout/reader/dot/text/DotTextReader.java
@@ -1,12 +1,18 @@
 package com.calpano.graphinout.reader.dot.text;
 
-import com.calpano.graphinout.base.gio.*;
+import com.calpano.graphinout.base.gio.GioData;
+import com.calpano.graphinout.base.gio.GioDocument;
+import com.calpano.graphinout.base.gio.GioEdge;
+import com.calpano.graphinout.base.gio.GioGraph;
+import com.calpano.graphinout.base.gio.GioNode;
+import com.calpano.graphinout.base.gio.GioWriter;
 import com.calpano.graphinout.base.input.InputSource;
 import com.calpano.graphinout.base.reader.GioFileFormat;
 import com.calpano.graphinout.base.reader.GioReader;
 import com.paypal.digraph.parser.GraphEdge;
 import com.paypal.digraph.parser.GraphNode;
 import com.paypal.digraph.parser.GraphParser;
+import org.slf4j.Logger;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -14,9 +20,11 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
 
-import static java.rmi.server.LogStream.log;
+import static org.slf4j.LoggerFactory.getLogger;
 
 public class DotTextReader implements GioReader {
+
+    private static final Logger log = getLogger(DotTextReader.class);
 
     @Override
     public void errorHandler(Consumer<ContentError> errorConsumer) {
@@ -42,42 +50,30 @@ public class DotTextReader implements GioReader {
         writer.startDocument(GioDocument.builder().build());
         writer.startGraph(GioGraph.builder().build());
 
-        log("--- nodes:");
+        log.info("--- nodes:");
         for (GraphNode node : nodes.values()) {
             List<GioData> gioDataList = new ArrayList<>();
             Map<String, Object> attributes = node.getAttributes();
             String nodeValue = String.valueOf(attributes.get("value"));
             String nodeKey = String.valueOf(attributes.get("key"));
-            GioData gioData = GioData.builder()
-                    .key(nodeKey)
-                    .value(nodeValue)
-                    .build();
+            GioData gioData = GioData.builder().key(nodeKey).value(nodeValue).build();
             gioDataList.add(gioData);
 
-            writer.startNode(GioNode.builder()
-                    .id(node.getId())
-                    .dataList(gioDataList)
-                    .build());
+            writer.startNode(GioNode.builder().id(node.getId()).dataList(gioDataList).build());
 
-            log(node.getId() + " " + node.getAttributes());
+            log.info(node.getId() + " " + node.getAttributes());
         }
 
-        log("--- edges:");
+        log.info("--- edges:");
         for (GraphEdge edge : edges.values()) {
             List<GioData> gioDataList = new ArrayList<>();
             Map<String, Object> attributes = edge.getAttributes();
             String edgeValue = String.valueOf(attributes.get("value"));
             String edgeKey = String.valueOf(attributes.get("key"));
-            GioData gioData = GioData.builder()
-                    .key(edgeKey)
-                    .value(edgeValue)
-                    .build();
+            GioData gioData = GioData.builder().key(edgeKey).value(edgeValue).build();
             gioDataList.add(gioData);
-            writer.startEdge(GioEdge.builder()
-                    .id(edge.getId())
-                    .dataList(gioDataList)
-                    .build());
-            log(edge.getNode1().getId() + "->" + edge.getNode2().getId() + " " + edge.getAttributes());
+            writer.startEdge(GioEdge.builder().id(edge.getId()).dataList(gioDataList).build());
+            log.info(edge.getNode1().getId() + "->" + edge.getNode2().getId() + " " + edge.getAttributes());
         }
         writer.endGraph();
         writer.endDocument();
