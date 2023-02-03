aa68cd6b359b0133e8c9bcba38bf2b3accded95c
Dr. Max Völkel <mv@maxvoelkel.de>
2023-02-03 11:45:19 +0100

    add simple xml writer and validating writer

---
+++ base/src/main/java/com/calpano/graphinout/base/output/xml/file/ValidatingXmlWriter.java
@@ -0,0 +1,53 @@
+package com.calpano.graphinout.base.output.xml.file;
+
+import com.calpano.graphinout.base.output.xml.XmlWriter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ * A chaining, validating {@link XmlWriter}.
+ */
+@Slf4j
+abstract class ValidatingXmlWriter implements XmlWriter {
+
+    protected final XmlWriter sink;
+    private final Stack<String> elements = new Stack<>();
+
+    public ValidatingXmlWriter(XmlWriter sink) {
+        this.sink = sink;
+    }
+
+    @Override
+    public void characterData(String characterData) throws IOException {
+        sink.characterData(characterData);
+    }
+
+    @Override
+    public void endDocument() throws IOException {
+        sink.endDocument();
+    }
+
+    @Override
+    public void endElement(String name) throws IOException {
+        String expected = elements.pop();
+        if (expected == null || !expected.equals(name)) {
+            throw new IllegalStateException("Expected close of element " + expected + " but got " + name);
+        }
+        sink.endElement(name);
+    }
+
+    @Override
+    public void startDocument() throws IOException {
+        sink.startDocument();
+    }
+
+    @Override
+    public void startElement(String name, Map<String, String> attributes) throws IOException {
+        elements.push(name);
+        sink.startElement(name, attributes);
+    }
+
+}
