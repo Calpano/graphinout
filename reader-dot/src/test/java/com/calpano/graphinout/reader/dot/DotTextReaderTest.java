e2f09a43c79fb0e6c0b4c0de7f1f9fc18e71624d
lilladiamantisz <ldiamantisz@le-soft.io>
2023-02-07 09:22:54 +0100

    add parameterized test for dot reader

--- reader-dot/src/test/java/com/calpano/graphinout/reader/dot/DotTextReaderTest.java
+++ reader-dot/src/test/java/com/calpano/graphinout/reader/dot/DotTextReaderTest.java
@@ -7,7 +7,8 @@ import com.calpano.graphinout.base.input.InputSource;
 import com.calpano.graphinout.base.output.OutputSink;
 import com.calpano.graphinout.base.output.xml.file.XMLFileWriter;
 import org.apache.commons.io.IOUtils;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -15,7 +16,9 @@ import java.nio.charset.StandardCharsets;
 class DotTextReaderTest {
 
     // TODO run on all available test files and check if parser crashes; later also validate GraphML?
-    @Test
+    @ParameterizedTest
+    @ValueSource(strings = {"/example.dot", "/example2.dot", "/example3.dot", "/example4.dot", "/example5.dot", "/example6.dot",
+            "/example7.dot", "/example8.dot", "/example9.dot", "/example10.dot", "/example11.dot", "/example12.dot",})
     void test() throws IOException {
         String resourceName = "/example.dot";
         String content = IOUtils.resourceToString(resourceName, StandardCharsets.UTF_8);
