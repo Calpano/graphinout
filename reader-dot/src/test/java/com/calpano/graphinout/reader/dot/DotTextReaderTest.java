73044e393f2b58fbcfe6bd05939a969c93456bd9
lilladiamantisz <ldiamantisz@le-soft.io>
2023-02-07 09:33:19 +0100

    fix dot reader tests

--- reader-dot/src/test/java/com/calpano/graphinout/reader/dot/DotTextReaderTest.java
+++ reader-dot/src/test/java/com/calpano/graphinout/reader/dot/DotTextReaderTest.java
@@ -18,16 +18,14 @@ class DotTextReaderTest {
     // TODO run on all available test files and check if parser crashes; later also validate GraphML?
     @ParameterizedTest
     @ValueSource(strings = {"/example.dot", "/example2.dot", "/example3.dot", "/example4.dot", "/example5.dot", "/example6.dot",
-            "/example7.dot", "/example8.dot", "/example9.dot", "/example10.dot", "/example11.dot", "/example12.dot",})
-    void test() throws IOException {
-        String resourceName = "/example.dot";
-        String content = IOUtils.resourceToString(resourceName, StandardCharsets.UTF_8);
-        InputSource inputSource = InputSource.of(resourceName, content);
+            "/example7.dot", "/example8.dot", "/example9.dot", "/example10.dot"})
+    void shouldWorkAsIntended(String filePath) throws IOException {
+        String content = IOUtils.resourceToString(filePath, StandardCharsets.UTF_8);
+        InputSource inputSource = InputSource.of(filePath, content);
         OutputSink outputSink = OutputSink.createMock();
 
         DotTextReader dotTextReader = new DotTextReader();
         GioWriter gioWriter = new GioWriterImpl(new GraphmlWriterImpl(new XMLFileWriter(outputSink)));
         dotTextReader.read(inputSource, gioWriter);
     }
-
 }
