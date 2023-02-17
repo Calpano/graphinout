d4a1716c98abb13a79d8985b0e59294fd7d3fe44
Dr. Max Völkel <mv@maxvoelkel.de>
2023-02-17 19:57:22 +0100

    fix compile error - oops

--- reader-jgrapht/src/test/java/com/calpano/graphinout/reader/jgrapht/Graph6ReaderTest.java
+++ reader-jgrapht/src/test/java/com/calpano/graphinout/reader/jgrapht/Graph6ReaderTest.java
@@ -2,14 +2,19 @@ package com.calpano.graphinout.reader.jgrapht;
 
 import com.calpano.graphinout.base.AbstractReaderTest;
 import com.calpano.graphinout.base.ReaderTests;
+import com.calpano.graphinout.base.output.InMemoryOutputSink;
 import com.calpano.graphinout.base.output.OutputSink;
 import com.calpano.graphinout.base.reader.GioReader;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 class Graph6ReaderTest extends AbstractReaderTest {
 
@@ -24,13 +29,17 @@ class Graph6ReaderTest extends AbstractReaderTest {
         GioReader gioReader = new Graph6Reader();
         ReaderTests.forEachReadableResource(gioReader, resourcePath -> {
             Writer w = null;
-            OutputSink outputSink = OutputSink.createForwarding(w);
+            InMemoryOutputSink outputSink = OutputSink.createInMemory();
             try {
                 ReaderTests.readResourceToSink(gioReader, resourcePath, outputSink, true, true, true);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
+            String s = new String( outputSink.getByteBuffer().toByteArray(), StandardCharsets.UTF_8 );
+            log.info("Read:\n"+s);
         });
     }
+
+    private static final Logger log = getLogger(Graph6ReaderTest.class);
 
 }
