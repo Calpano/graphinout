ca5fcdd39e70fc4883dfebb01088d32ebac02600
Dr. Max Völkel <mv@maxvoelkel.de>
2023-02-17 19:53:25 +0100

    fix graph6 reader

--- reader-jgrapht/src/main/java/com/calpano/graphinout/reader/jgrapht/Graph6Reader.java
+++ reader-jgrapht/src/main/java/com/calpano/graphinout/reader/jgrapht/Graph6Reader.java
@@ -14,12 +14,15 @@ import com.calpano.graphinout.base.reader.GioReader;
 import org.jgrapht.Graph;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.DefaultUndirectedGraph;
+import org.jgrapht.nio.AttributeType;
 import org.jgrapht.nio.ImportException;
 import org.jgrapht.nio.graph6.Graph6Sparse6Importer;
 import org.slf4j.Logger;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Consumer;
 
 import static org.slf4j.LoggerFactory.getLogger;
@@ -36,7 +39,7 @@ public class Graph6Reader implements GioReader {
 
     @Override
     public GioFileFormat fileFormat() {
-        return new GioFileFormat("graph6", "graph6 format", ".g6",".graph6");
+        return new GioFileFormat("graph6", "graph6 format", ".g6", ".graph6");
     }
 
     @Override
@@ -49,9 +52,21 @@ public class Graph6Reader implements GioReader {
         // here we read the inputStream of sis ...
 
         // TODO streaming
-        Graph6Sparse6Importer<String, DefaultEdge> importer = new Graph6Sparse6Importer<>();
-        importer.setVertexFactory( i -> ""+i);
-        Graph<String, DefaultEdge> graph = new DefaultUndirectedGraph<>(DefaultEdge.class);
+        Graph6Sparse6Importer<Integer, DefaultEdge> importer = new Graph6Sparse6Importer<>();
+        Map<Integer, String> nodeIds = new HashMap<>();
+        importer.addVertexAttributeConsumer((pair, att) -> {
+            Integer vertex = pair.getFirst();
+            String s = pair.getSecond();
+            AttributeType attType = att.getType();
+            String attValue = att.getValue();
+            if (s.equals("ID")) {
+                nodeIds.put(vertex, attValue);
+            } else {
+                log.info("Read " + vertex + "." + s + " = " + attValue + " of " + attType);
+            }
+        });
+        importer.setVertexFactory(i -> i);
+        Graph<Integer, DefaultEdge> graph = new DefaultUndirectedGraph<>(DefaultEdge.class);
         try {
             importer.importGraph(graph, sis.inputStream());
 
@@ -59,18 +74,28 @@ public class Graph6Reader implements GioReader {
             writer.startDocument(GioDocument.builder().build());
             writer.startGraph(GioGraph.builder().build());
 
-            for (String v : graph.vertexSet()) {
-                writer.startNode(GioNode.builder().id(v).build());
+            for (Integer v : graph.vertexSet()) {
+                String nodeId = nodeIds.get(v);
+                writer.startNode(GioNode.builder().id(nodeId).build());
                 writer.endNode(null);
             }
             for (DefaultEdge e : graph.edgeSet()) {
-                String source = graph.getEdgeSource(e);
-                String target = graph.getEdgeTarget(e);
-                writer.startEdge(GioEdge.builder().endpoints(Arrays.asList(GioEndpoint.builder().id(source).build(), GioEndpoint.builder().id(target).build())).build());
+                Integer source = graph.getEdgeSource(e);
+                Integer target = graph.getEdgeTarget(e);
+                String s = nodeIds.get(source);
+                String t = nodeIds.get(target);
+                if(s == null) {
+                    throw new IllegalStateException("No nodeId for "+source);
+                }
+                if(t == null) {
+                    throw new IllegalStateException("No nodeId for "+target);
+                }
+                writer.startEdge(GioEdge.builder().endpoints(Arrays.asList( //
+                        GioEndpoint.builder().node(s).build(), //
+                        GioEndpoint.builder().node(t).build() //
+                )).build());
                 writer.endEdge();
             }
-
-            // content errors can be signaled like this
 
             writer.endGraph(null);
             writer.endDocument();
--- reader-jgrapht/src/test/java/com/calpano/graphinout/reader/jgrapht/Graph6ReaderTest.java
+++ reader-jgrapht/src/test/java/com/calpano/graphinout/reader/jgrapht/Graph6ReaderTest.java
@@ -1,8 +1,13 @@
 package com.calpano.graphinout.reader.jgrapht;
 
 import com.calpano.graphinout.base.AbstractReaderTest;
+import com.calpano.graphinout.base.ReaderTests;
+import com.calpano.graphinout.base.output.OutputSink;
 import com.calpano.graphinout.base.reader.GioReader;
+import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.io.Writer;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,6 +16,21 @@ class Graph6ReaderTest extends AbstractReaderTest {
     @Override
     protected List<GioReader> readersToTest() {
         return Arrays.asList(new Graph6Reader());
+    }
+
+    @Test
+    void test() {
+        // stream incoming test resource as XML to the logger
+        GioReader gioReader = new Graph6Reader();
+        ReaderTests.forEachReadableResource(gioReader, resourcePath -> {
+            Writer w = null;
+            OutputSink outputSink = OutputSink.createForwarding(w);
+            try {
+                ReaderTests.readResourceToSink(gioReader, resourcePath, outputSink, true, true, true);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 
 }
