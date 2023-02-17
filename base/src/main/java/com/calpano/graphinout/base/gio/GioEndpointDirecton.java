45d5bcb205bf76d61f6f0e50cba79871590ad235
Dr. Max Völkel <mv@maxvoelkel.de>
2023-02-17 19:51:17 +0100

    extend GioEndpointDirection

--- base/src/main/java/com/calpano/graphinout/base/gio/GioEndpointDirecton.java
+++ base/src/main/java/com/calpano/graphinout/base/gio/GioEndpointDirecton.java
@@ -2,5 +2,14 @@ package com.calpano.graphinout.base.gio;
 
 public enum GioEndpointDirecton {
 
-    In, Out, Undirected
+    In, Out, Undirected;
+
+    public boolean isDirected() {
+        return this == In || this == Out;
+    }
+
+    public boolean isUndirected() {
+        return this == Undirected;
+    }
+
 }
