e60d28c75d3b9f7cb116222b10abe259ccd5c158
Dr. Max Völkel <mv@maxvoelkel.de>
2023-04-25 11:42:58 +0200

    fixes #51

--- base/src/main/java/com/calpano/graphinout/base/xml/XmlWriterImpl.java
+++ base/src/main/java/com/calpano/graphinout/base/xml/XmlWriterImpl.java
@@ -3,6 +3,7 @@ package com.calpano.graphinout.base.xml;
 import com.calpano.graphinout.base.output.OutputSink;
 import lombok.extern.slf4j.Slf4j;
 
+import javax.annotation.Nullable;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
@@ -25,12 +26,14 @@ public class XmlWriterImpl implements XmlWriter {
 
     @Override
     public void characterData(String characterData) throws IOException {
+        writeBufferMaybe();
         log.trace("characterData [{}]", characterData);
         writer.write(characterData);
     }
 
     @Override
     public void endDocument() throws IOException {
+        writeBufferMaybe();
         log.trace("endDocument");
         writer.write("\n");
         this.writer.flush();
@@ -42,9 +45,20 @@ public class XmlWriterImpl implements XmlWriter {
     @Override
     public void endElement(String name) throws IOException {
         log.trace("endElement [{}]", name);
-        writer.write("</");
-        writer.write(name);
-        writer.write(">");
+        if(this.name!=null) {
+            writer.write("<");
+            writer.write(name);
+            for (Map.Entry<String, String> entry : attributes.entrySet()) {
+                writer.write(" " + entry.getKey() + "=\"" + entry.getValue() + "\"");
+            }
+            writer.write("/>");
+            this.name = null;
+            this.attributes = null;
+        } else {
+            writer.write("</");
+            writer.write(name);
+            writer.write(">");
+        }
     }
 
     @Override
@@ -54,20 +68,39 @@ public class XmlWriterImpl implements XmlWriter {
         this.writer = new OutputStreamWriter(out, StandardCharsets.UTF_8);
     }
 
+    private @Nullable String name = null;
+    private @Nullable Map<String,String> attributes = null;
+
+
     @Override
     public void startElement(String name, Map<String, String> attributes) throws IOException {
         log.trace("startElement [{}]", name);
-        writer.write("<");
-        writer.write(name);
-        for (Map.Entry<String, String> entry : attributes.entrySet()) {
-            writer.write(" " + entry.getKey() + "=\"" + entry.getValue() + "\"");
+        writeBufferMaybe();
+        buffer(name, attributes);
+    }
+
+    private void buffer(String name, Map<String, String> attributes) {
+        this.name=name;
+        this.attributes = attributes;
+    }
+
+    private void writeBufferMaybe() throws IOException {
+        if(this.name != null) {
+            writer.write("<");
+            writer.write(name);
+            for (Map.Entry<String, String> entry : attributes.entrySet()) {
+                writer.write(" " + entry.getKey() + "=\"" + entry.getValue() + "\"");
+            }
+            writer.write('>');
+            writer.flush();
         }
-        writer.write('>');
-        writer.flush();
+        this.name= null;
+        this.attributes = null;
     }
 
     @Override
     public void lineBreak() throws IOException {
+        writeBufferMaybe();
         writer.write("\n");
     }
 
