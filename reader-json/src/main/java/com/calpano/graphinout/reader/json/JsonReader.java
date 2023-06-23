7b03c82568a4e99dcac2fc192db15cc28c72626a
Dr. Max Völkel <mv@maxvoelkel.de>
2023-06-23 11:01:44 +0200

    prepare reading data + mapping

--- reader-json/src/main/java/com/calpano/graphinout/reader/json/JsonReader.java
+++ reader-json/src/main/java/com/calpano/graphinout/reader/json/JsonReader.java
@@ -1,7 +1,10 @@
 package com.calpano.graphinout.reader.json;
 
 import com.calpano.graphinout.base.gio.GioWriter;
+import com.calpano.graphinout.base.input.FilesMultiInputSource;
 import com.calpano.graphinout.base.input.InputSource;
+import com.calpano.graphinout.base.input.MultiInputSource;
+import com.calpano.graphinout.base.input.SingleInputSource;
 import com.calpano.graphinout.base.reader.ContentError;
 import com.calpano.graphinout.base.reader.GioFileFormat;
 import com.calpano.graphinout.base.reader.GioReader;
@@ -10,6 +13,8 @@ import java.io.IOException;
 import java.util.function.Consumer;
 
 public class JsonReader implements GioReader {
+    public static final String DATA = "data";
+    public static final String MAPPING = "mapping";
     private Consumer<ContentError> errorHandler;
     @Override
     public void errorHandler(Consumer<ContentError> errorHandler) {
@@ -23,6 +28,13 @@ public class JsonReader implements GioReader {
 
     @Override
     public void read(InputSource inputSource, GioWriter writer) throws IOException {
-
+        if(inputSource.isMulti()) {
+            MultiInputSource multiInputSource = (MultiInputSource) inputSource;
+            SingleInputSource dataSource = multiInputSource.getNamedSource(DATA);
+            SingleInputSource mappingSource = multiInputSource.getNamedSource(MAPPING);
+            // TODO read data and mapping
+        } else {
+            throw new IOException("JsonReader expects a multi-input for data and mapping");
+        }
     }
 }
