a60f1a571056f9e187886e30a2d38b616cdddce7
Dr. Max Völkel <mv@maxvoelkel.de>
2025-06-23 12:56:07 +0200

    simplify project

--- app-cmdline/src/main/java/com/calpano/graphinout/app/cmdline/Main.java
+++
@@ -1,7 +0,0 @@
-package com.calpano.graphinout.app.cmdline;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- app-rest-service/src/main/java/com/calpano/graphinout/apprestservice/GioMediaType.java
+++
@@ -1,7 +0,0 @@
-package com.calpano.graphinout.apprestservice;
-
-public class GioMediaType {
-
-    public static final String APPLICATION_ZIP_VALUE = "application/zip";
-
-}
