0b4a36ad2ff31e5f519b673c512772b677741613
Dr. Max Völkel <mv@maxvoelkel.de>
2025-06-24 08:17:55 +0200

    initial validating gioWriter

---
+++ base/src/main/java/com/calpano/graphinout/base/validation/GioWriterEndException.java
@@ -0,0 +1,12 @@
+package com.calpano.graphinout.base.validation;
+
+public class GioWriterEndException extends GioWriterException {
+
+
+
+    public GioWriterEndException(ValidatingGioWriter.CurrentElement offendingElement, ValidatingGioWriter.CurrentElement lastStartedElement, String msg) {
+        super(offendingElement,lastStartedElement,msg);
+
+    }
+
+}
