3e2e0e8ebc5e9bc3d12bb462b422f4b740eb70a4
Dr. Max Völkel <mv@maxvoelkel.de>
2025-07-15 11:48:04 +0200

    add CjConstants

---
+++ base/src/main/java/com/calpano/graphinout/base/cj/CjConstants.java
@@ -0,0 +1,54 @@
+package com.calpano.graphinout.base.cj;
+
+public class CjConstants {
+
+    /** Graph base URI for RDF interpretation */
+    public static final String BASE_URI = "baseUri";
+
+    /** Graph */
+    public static final String CANONICAL = "canonical";
+
+    /** Data */
+    public static final String DATA = "data";
+    /** Edge endpoint direction (in/out/undir) */
+    public static final String DIRECTION = "direction";
+    /** Graph edges */
+    public static final String EDGES = "edges";
+    /** Edge endpoints */
+    public static final String ENDPOINTS = "endpoints";
+    /** Node nested graphs, Edge nested graphs */
+    public static final String GRAPHS = "graphs";
+    /** Node id, Edge id, Graph id, Port id */
+    public static final String ID = "id";
+    /** Node, Edge, Graph, Port */
+    public static final String LABEL = "label";
+    /** Label */
+    public static final String LANGUAGE = "language";
+    /** Graph */
+    public static final String META = "meta";
+    /** Edge Endpoint referenced node id */
+    public static final String NODE = "node";
+    /** Graph nodes */
+    public static final String NODES = "nodes";
+    /** Edge Endpoint referenced port id */
+    public static final String PORT = "port";
+    /** Node ports */
+    public static final String PORTS = "ports";
+    /** Edge, Edge Endpoint */
+    public static final String TYPE = "type";
+    /** Edge, Edge Endpoint */
+    public static final String TYPE_NODE = "typeNode";
+    /** Edge, Edge Endpoint */
+    public static final String TYPE_URI = "typeUri";
+    /** Label */
+    public static final String VALUE = "value";
+
+    public static final String VERSION_DATE = "versionDate";
+    public static final String VERSION_NUMBER = "versionNumber";
+    public static final String CONNECTED_JSON = "connectedJson";
+
+    private CjConstants() {
+        // hidden constructor
+    }
+
+}
