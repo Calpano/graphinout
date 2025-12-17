7fc3986c9756b4be461c177aec0a0294badffcb7
Dr. Max Völkel <mv@maxvoelkel.de>
2025-12-17 08:04:55 +0100

    doc

--- base/src/main/java/com/graphinout/base/json/JavaJsons.java
+++ base/src/main/java/com/graphinout/base/json/JavaJsons.java
@@ -1,10 +1,15 @@
 package com.graphinout.base.json;
 
+import com.graphinout.foundation.pure.collections.jajson.JaJson;
 import com.graphinout.foundation.pure.json.document.IJsonValue;
 import com.graphinout.foundation.pure.json.writer.impl.Json2JavaJsonWriter;
 
 import java.io.IOException;
 
+/**
+ * Java-based implementation of {@link IJsonValue} API, as opposed ot the Jackson-basewd implementation.
+ * Do not confuse with the {@link JaJson} API.
+ */
 public class JavaJsons {
 
     public static IJsonValue ofJsonString(String jsonString) {
