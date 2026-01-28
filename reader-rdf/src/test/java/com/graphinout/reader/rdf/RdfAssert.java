13028a4d3cd2e2879eb5a9269c311b10217f198f
Dr. Max Völkel <mv@maxvoelkel.de>
2026-01-28 10:57:35 +0100

    just use Jenas isomorphicModel

--- reader-rdf/src/test/java/com/graphinout/reader/rdf/RdfAssert.java
+++ reader-rdf/src/test/java/com/graphinout/reader/rdf/RdfAssert.java
@@ -2,35 +2,32 @@ package com.graphinout.reader.rdf;
 
 import org.apache.jena.rdf.model.Model;
 import org.jspecify.annotations.Nullable;
+import org.slf4j.Logger;
 
-import static com.google.common.truth.Truth.assertThat;
+import static org.slf4j.LoggerFactory.getLogger;
 
 public class RdfAssert {
 
+    private static final Logger log = getLogger(RdfAssert.class);
 
-    public static boolean xAssertThatIsSameRdf(Model actualRdfDoc, Model expectedRdfDoc, @Nullable Runnable extendedDebugInfos
-    ) {
-        String actualJson = RdfModels.toRdfNQuads(actualRdfDoc);
-        String expectedJson = RdfModels.toRdfNQuads(expectedRdfDoc);
-        return xAssertThatIsSameRdf(actualJson, expectedJson, extendedDebugInfos);
+    public static boolean xAssertThatIsSameRdf(Model actualRdfDoc, Model expectedRdfDoc, @Nullable Runnable extendedDebugInfos) {
+        if (actualRdfDoc.isIsomorphicWith(expectedRdfDoc)) {
+            return true;
+        }
+        if (extendedDebugInfos != null) {
+            extendedDebugInfos.run();
+        }
+        Model unexpected = actualRdfDoc.difference(expectedRdfDoc);
+        Model missing = expectedRdfDoc.difference(actualRdfDoc);
+        log.info("--- RDF expected, but missing:\n----\n" + RdfModels.toRdfNQuads(missing));
+        log.info("--- RDF unexpected, but present:\n----\n" + RdfModels.toRdfNQuads(unexpected));
+        return false;
     }
 
     public static boolean xAssertThatIsSameRdf(String actualRdfNQuads, String expectedRdfNQuads, @Nullable Runnable extendedDebugInfos) {
-        String expectedWrapped = RdfModels.normalize(actualRdfNQuads);
-        String actualWrapped = RdfModels.normalize(expectedRdfNQuads);
-
-        if (extendedDebugInfos != null && !actualWrapped.equals(expectedWrapped)) {
-            extendedDebugInfos.run();
-        }
-
-        // prevent out of memory for large files
-        if (actualRdfNQuads.length() < 1024 * 1024) {
-            assertThat(actualWrapped).isEqualTo(expectedWrapped);
-        } else {
-            assertThat(actualWrapped.equals(expectedWrapped)).isTrue();
-        }
-        //test failed before, if it failed
-        return true;
+        Model actual = RdfModels.ofRdfNQuads(actualRdfNQuads);
+        Model expected = RdfModels.ofRdfNQuads(expectedRdfNQuads);
+        return xAssertThatIsSameRdf(actual, expected, extendedDebugInfos);
     }
 
 }
