d5f3ac1558cbfbc7c543946ae0e1ef8798066a91
Dr. Max Völkel <mv@maxvoelkel.de>
2026-07-06 14:43:58 +0200

    Refactor `TestFileProviderTest` to clarify resource availability and add assumptions for external resources.

--- testdata/src/test/java/com/graphinout/testdata/TestFileProviderTest.java
+++ testdata/src/test/java/com/graphinout/testdata/TestFileProviderTest.java
@@ -3,18 +3,22 @@ package com.graphinout.testdata;
 import com.google.common.truth.Truth;
 import org.junit.jupiter.api.Test;
 
-import static com.google.common.truth.Truth.assertThat;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 class TestFileProviderTest {
 
     @Test
     void testResourceLoading() {
+        // Always available: in-repo JSON resources and in-memory JSON5 inputs
         Truth.assertThat(TestFileProvider.getAllTestResources()).isNotEmpty();
+        Truth.assertThat(TestFileProvider.jsonResources()).isNotEmpty();
+        Truth.assertThat(TestFileProvider.json5InputSources()).isNotEmpty();
+
+        // These resource types only exist in the external graph-test-data repo
+        assumeTrue(TestFileUtil.externalRoot() != null, "external graph-test-data not available");
         Truth.assertThat(TestFileProvider.graphmlResources()).isNotEmpty();
         Truth.assertThat(TestFileProvider.xmlResources()).isNotEmpty();
         Truth.assertThat(TestFileProvider.cjResourcesCanonical()).isNotEmpty();
-        Truth.assertThat(TestFileProvider.jsonResources()).isNotEmpty();
-        Truth.assertThat(TestFileProvider.json5InputSources()).isNotEmpty();
     }
 
 }
